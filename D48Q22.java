764fc36c2ec3b9f54ef9cad95fe6df53b2e6ca07
itc-s24009 <s24009@std.it-college.ac.jp>
2025-05-16 12:38:51 +0900

    D48(3)

--- D48Q22.java
+++ D48Q22.java
@@ -1,7 +1,7 @@
 public class D48Q22 {
     public static void main(String[] args) {
         enum Connection {OFFLINE, ONLINE}
-        Connection s = OFFLINE;
+        Connection s = Connection.OFFLINE;
         if (s == Connection.OFFLINE) {
             s = Connection.ONLINE;
         }
